8a01964a22cd29436ddf279ceffebe1ff1852f68
inhasid <sidorchuk.inga@gmail.com>
2023-12-23 19:55:25 +0100

    profile-js-css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import './app.css';
+import user from '../data/user.json';
+import Profile from './Profile/Profile';
+
+const {
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+} = user;
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="divWrap">
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        followers={followers}
+        views={views}
+        likes={likes}
+      />
     </div>
+
   );
 };
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import styles from "./profile.module.css";
+
+const Profile = ({
+    username,
+    tag,
+    location,
+    avatar,
+    followers,
+    views,
+    likes,
+}) => {
+    return (
+        <div className={styles.profile}>
+            <div className={styles.description}>
+                <img src={avatar} alt="User avatar" className={styles.avatar} />
+                <p className={styles.name}>{username}</p>
+                <p className={styles.tag}>@{tag}</p>
+                <p className={styles.location}>{location}</p>
+            </div>
+
+            <ul className={styles.stats}>
+                <li>
+                    <span className={styles.label}>Followers</span>
+                    <span className={styles.quantity}>{followers}</span>
+                </li>
+                <li>
+                    <span className={styles.label}>Views</span>
+                    <span className={styles.quantity}>{views}</span>
+                </li>
+                <li>
+                    <span className={styles.label}>Likes</span>
+                    <span className={styles.quantity}>{likes}</span>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Profile;
